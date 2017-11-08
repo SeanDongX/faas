ad9caa94422a646b6b581df02814a7fca72a3e1e
Sean <wonderxboy@gmail.com>
2017-11-09 00:57:18 +0800

    add cs sample function to echo request query string and header

---
+++ sample-functions/CsEcho/src/FunctionHandler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text;
+
+namespace Function
+{
+    public class FunctionHandler
+    {
+        public void Handle(string input) {
+            var queryString = Environment.GetEnvironmentVariable("Http_Query");
+            var forwardHeader = Environment.GetEnvironmentVariable("Http_X_Forwarded_By");
+
+            Console.WriteLine(
+                string.Format("Request query string: '{0}' , forward header: '{1}'",
+                queryString,
+                forwardHeader));
+        }
+    }
+}
